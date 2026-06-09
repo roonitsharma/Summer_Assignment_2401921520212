db4e18c8b86e1b163fdb8c585e354c3ccd55b444
Roonit Sharma <roonitsharma4@gmail.com>
2026-06-09 16:46:02 +0530

    Create PermutationInStrings.java
    Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
    
    In other words, return true if one of s1's permutations is the substring of s2.
    
     
    
    Example 1:
    
    Input: s1 = "ab", s2 = "eidbaooo"
    Output: true
    Explanation: s2 contains one permutation of s1 ("ba").
    Example 2:
    
    Input: s1 = "ab", s2 = "eidboaoo"
    Output: false

---
+++ Week2/Day2/PermutationInStrings.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        if (n > m) return false;
+
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+
+        for (int i = 0; i < n; i++) {
+            freq1[s1.charAt(i) - 'a']++;
+            freq2[s2.charAt(i) - 'a']++;
+        }
+
+        if (isEqual(freq1, freq2)) return true;
+
+        for (int i = n; i < m; i++) {
+            freq2[s2.charAt(i) - 'a']++;
+            freq2[s2.charAt(i - n) - 'a']--;
+
+            if (isEqual(freq1, freq2)) return true;
+        }
+
+        return false;
+    }
+
+    private boolean isEqual(int[] a, int[] b) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) return false;
+        }
+        return true;
+    }
+}
