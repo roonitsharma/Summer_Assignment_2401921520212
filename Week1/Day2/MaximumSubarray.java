db21d62ef2a76757005a75d4b33aeb4c4117c44b
Roonit Sharma <roonitsharma4@gmail.com>
2026-06-12 12:38:42 +0530

    Create MaximumSubarray.java
    Given an integer array nums, find the subarray with the largest sum, and return its sum.
    
     
    
    Example 1:
    
    Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
    Output: 6
    Explanation: The subarray [4,-1,2,1] has the largest sum 6.
    Example 2:
    
    Input: nums = [1]
    Output: 1
    Explanation: The subarray [1] has the largest sum 1.
    Example 3:
    
    Input: nums = [5,4,-1,7,8]
    Output: 23
    Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.

---
+++ Week1/Day2/MaximumSubarray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums.length==1) {
+            return nums[0];
+        }
+        int maxsum = Integer.MIN_VALUE;
+        int sum=0;
+        for(int i=0;i<nums.length;i++) {
+           sum +=nums[i];
+           if(sum > maxsum) {
+            maxsum=sum;
+           }
+           if (sum < 0) {
+                sum = 0; 
+            }
+        }
+        return maxsum;
+    }
+}  
